0a8413660f191ca177073246d004126474d8681d
Ian Canário <ian.canario@gmail.com>
2026-07-28 10:29:24 -0300

    chore: config database

---
+++ src/common/database/data-source.ts
@@ -0,0 +1,24 @@
+import { config } from "dotenv";
+import { join } from "path";
+import { DataSourceOptions } from "typeorm";
+
+config()
+const isProd = process.env.NODE_ENV === 'production';
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT ?? '5432', 10),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+
+  entities: [],
+  migrations: [
+    isProd
+      ? join(process.cwd(), 'dist/database/migrations/*.js')
+      : join(process.cwd(), 'src/database/migrations/*.ts'),
+  ],
+
+  synchronize: true,
+};
