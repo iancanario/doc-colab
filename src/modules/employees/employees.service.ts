df82c3283dbde08b35fd75390d52864efdc43f2f
Ian Canário <ian.canario@gmail.com>
2026-07-31 15:21:00 -0300

    refact(employees): remove try/catch

--- src/modules/employees/employees.service.ts
+++ src/modules/employees/employees.service.ts
@@ -1,8 +1,4 @@
-import {
-  Inject,
-  Injectable,
-  InternalServerErrorException,
-} from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreateEmployeeDTO } from './dtos/create-employee.dto';
 import { IEmployeesService } from './interfaces/employees-service.intreface';
 import { IEmployeesRepository } from './interfaces/employees-repository.interface';
@@ -19,59 +15,28 @@ export class EmployeesService implements IEmployeesService {
   async createEmployee(
     employee: CreateEmployeeDTO,
   ): Promise<{ message: string }> {
-    try {
-      await this.employeesRepository.createEmployee(employee);
-      return { message: 'Create employee successfully' };
-    } catch (error) {
-      throw new InternalServerErrorException('Error to create employee', {
-        cause: error,
-      });
-    }
+    await this.employeesRepository.createEmployee(employee);
+    return { message: 'Create employee successfully' };
   }
 
   async findEmployees(): Promise<Employee[]> {
-    try {
-      return await this.employeesRepository.findEmployees();
-    } catch (error) {
-      throw new InternalServerErrorException('Error to find employees', {
-        cause: error,
-      });
-    }
+    return await this.employeesRepository.findEmployees();
   }
 
   async findEmployeeById(id: string): Promise<Employee | null> {
-    try {
-      return await this.employeesRepository.findEmployeeById(id);
-    } catch (error) {
-      throw new InternalServerErrorException('Error to find employee', {
-        cause: error,
-      });
-    }
+    return await this.employeesRepository.findEmployeeById(id);
   }
 
   async updateEmployee(
     id: string,
     update: UpdateEmployeeDTO,
   ): Promise<{ message: string }> {
-    try {
-      await this.employeesRepository.updateEmployee(id, update);
-      return { message: 'Update employee successfully' };
-    } catch (error) {
-      throw new InternalServerErrorException('Error to find employee', {
-        cause: error,
-      });
-    }
+    await this.employeesRepository.updateEmployee(id, update);
+    return { message: 'Update employee successfully' };
   }
 
   async deleteEmployee(id: string): Promise<{ message: string }> {
-    try {
-      await this.employeesRepository.deleteEmployee(id);
-      return { message: 'Delete employee successfully' };
-    } catch (error) {
-      console.log(error);
-      throw new InternalServerErrorException('Error to delete employee', {
-        cause: error,
-      });
-    }
+    await this.employeesRepository.deleteEmployee(id);
+    return { message: 'Delete employee successfully' };
   }
 }
